dd6d94e8d76ecc723432c2fd10ecbd355c78664c
Cyphonvoid <yashaswi.kul@gmail.com>
2024-07-04 23:15:51 -0700

    commit newss

--- api/index.js
+++ api/index.js
@@ -4,6 +4,5 @@ import { expressServer } from "../Server/server_tools";
 
 expressServer.set_port(3000)
 expressServer.use_cors(false)
-expressServer.update_router({router_url:"/api"})
 expressServer.start()
 module.exports = expressServer.router('app')
