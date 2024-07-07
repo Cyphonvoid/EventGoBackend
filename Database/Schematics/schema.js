2495c574b46e66646824d0c9b8da9422502a362f
yashaswi <yashaswi.kul@gmail.com>
2024-07-07 14:14:00 -0700

    commitaslikudhj

--- Database/Schematics/schema.js
+++ Database/Schematics/schema.js
@@ -4,11 +4,12 @@ import { Show } from "./Show.js";
 import { Ticket } from "./Ticket.js";
 import { Transaction } from "./Transaction.js";
 import { SupabaseUser, EventGoUser, CombinedUser} from "./User.js";
+import { TicketQRCode } from "./TicketQRCode.js";
 import { BaseEntity } from "./BaseEntity.js";
 
 
 export {
     supabaseAdminClient, supabaseClient,
-    EventGoBusiness, Show, Ticket, Transaction, SupabaseUser, EventGoUser, CombinedUser,
+    EventGoBusiness, Show, Ticket, Transaction, SupabaseUser, EventGoUser, CombinedUser, TicketQRCode,
     BaseEntity
 }
