619315f1bc0b4577048651e45a53d1531255fea0
Cyphonvoid <yashaswi.kul@gmail.com>
2024-07-16 02:36:50 -0700

    asdas

--- Server/Webhooks/payment.js
+++ Server/Webhooks/payment.js
@@ -12,21 +12,24 @@ export async function AddPaymentMethod(req, res){
 expressServer.router('app').post('/stripeTransaction', StripeGateway)
 async function StripeGateway(req, res){
 
-        console.log(req.body, "this is request body");
+        //let data = req.body
+        let data = req.body.record
+
+        console.log(data, "this is request body");
         //call the stripe api to process payment
         let stripe_response = true;
 
         if(stripe_response == false){
-            let processed_ticket = await database.eventgo_schema().ProcessedTicket(req.body)
+            let processed_ticket = await database.eventgo_schema().ProcessedTicket(data)
             let deleted = await processed_ticket.Delete();
             if(deleted){var response = " Ticket removed from processed_tickets table"}
             res.send("Stripe transaction processing failed. Didn't create resources in database" + response)
             return false;
         }
 
-        let processed_ticket = await database.eventgo_schema().ProcessedTicket(req.body)
+        let processed_ticket = await database.eventgo_schema().ProcessedTicket(data)
         let generated = await processed_ticket.GenerateTransaction();
-        let deleted = await database.eventgo_schema().Ticket(req.body).Delete();
+        let deleted = await database.eventgo_schema().Ticket(data).Delete();
 
         console.log("generated", generated, " deleted", deleted)
         if(generated == false || deleted == false){
